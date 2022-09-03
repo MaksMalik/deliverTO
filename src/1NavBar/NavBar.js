18821f39273ae2e043346468651bc5abd1599cee
MaksMalik <maksymilian.malik@gmail.com>
2022-09-03 22:38:06 +0200

    Edit mobile navbar

--- src/1NavBar/NavBar.js
+++ src/1NavBar/NavBar.js
@@ -5,7 +5,8 @@ function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-logo">
-        Delivery
+        <p className="navbar-logo-deliver">deliver</p>
+        <p className="navbar-logo-to">TO</p>
       </div>
       <div className="navbar-links">
         <p><a className="navbar-links-link" href="#WhyUs">Why us</a></p>
