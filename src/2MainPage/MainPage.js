6676fc781fa0bdd07faaebeea14a4f0b37d48e71
MaksMalik <maksymilian.malik@gmail.com>
2022-09-04 14:41:50 +0200

    change why us, add dots

--- src/2MainPage/MainPage.js
+++ src/2MainPage/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './MainPage.css'
 import MainPageStart from './MainPageStart/MainPageStart'
+import WhyUs from './MainPageWhyUs/WhyUs'
 
 function MainPage() {
   return (
     <div className="mainPage">
       <MainPageStart></MainPageStart>
+      <WhyUs></WhyUs>
     </div>
 
   )
---
+++ src/2MainPage/MainPageWhyUs/WhyUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './WhyUs.css'
+
+function WhyUs() {
+  return (
+    <>
+    <div className="mainPageWhyUs-dots">
+      <div className="mainPageWhyUs-dot1"></div>
+      <div className="mainPageWhyUs-dot2"></div>
+      <div className="mainPageWhyUs-dot3"></div>
+    </div>
+    <div className="mainPageWhyUs">
+      <div className="mainPageWhyUs-title" id="WhyUs">Why us?</div>
+    </div>
+    </>
+  )
+}
+
+export default WhyUs
