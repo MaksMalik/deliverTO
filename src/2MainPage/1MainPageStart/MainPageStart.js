962b5470017c83dbb7419810af665f3ff7b872dd
MaksMalik <maksymilian.malik@gmail.com>
2022-09-13 15:45:01 +0200

    add begin button

--- src/2MainPage/1MainPageStart/MainPageStart.js
+++ src/2MainPage/1MainPageStart/MainPageStart.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import './MainPageStart.css'
 
-function MainPageStart() {
+function MainPageStart () {
   return (
-    <div className="mainPageStart">
-      <img className="mainPageStart-image" src="https://i.ibb.co/MPxcVWS/5227.jpg" alt="Delivery men"></img>
+    <div className='mainPageStart'>
+      <img
+        className='mainPageStart-image'
+        src='./images/5227.jpg'
+        alt='Delivery men'
+      ></img>
 
       <div>
-        <p className="mainPageStart-title">
-          Deliver with us
+        <p className='mainPageStart-title'>Deliver with us</p>
+        <p className='mainPageStart-getStarted'>
+          <a href='#WhyUs'>GET STARTED</a>
         </p>
-        <p className="mainPageStart-getStarted"><a href="#WhyUs">GET STARTED</a></p>
       </div>
     </div>
-
   )
 }
 
